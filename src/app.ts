6e880ffe4418ee3537191f73df56efffe58c662a
Mamadou Diouf <papadiouf93@gmail.com>
2024-08-26 18:27:06 +0000

    cors

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ class App {
 
     middleware() {
         this.server.use(cors({
-            origin: 'http://localhost:5174', 
+            origin: 'http://localhost:5173', 
             credentials: true,
         }));
         this.server.use(express.json());
